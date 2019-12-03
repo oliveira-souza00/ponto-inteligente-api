5511eabf0eff846100ea33e9f3258ffb1e5db890
Oliveira Souza <oliveirasouza00@gmail.com>
2019-12-02 21:58:14 -0300

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/com/sts/pontointeligente/api/PontoIntelegenteApplication.java
@@ -0,0 +1,13 @@
+package com.sts.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIntelegenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoIntelegenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sts/pontointeligente/api/PontoIntelegenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sts.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoIntelegenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
